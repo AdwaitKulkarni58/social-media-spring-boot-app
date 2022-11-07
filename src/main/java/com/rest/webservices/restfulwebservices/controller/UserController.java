97dd30f89a1014b3b3fe9c375ab0c2fc87942df2
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-11-07 12:37:05 -0800

    First commit

---
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.rest.webservices.restfulwebservices.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rest.webservices.restfulwebservices.service.UserDaoService;
+import com.rest.webservices.restfulwebservices.user.User;
+
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserDaoService userDaoService;
+	
+	public UserController(UserDaoService userDaoService) {
+		this.userDaoService = userDaoService;
+	}
+
+	@GetMapping(path = "/users")
+	public List<User> getUsers() {
+		return userDaoService.findAll(); 
+	}
+	
+	@GetMapping(path = "/users/{id}")
+	public User getUser(@PathVariable int id) {
+		return userDaoService.findOne(id);
+	}
+	
+}
---
+++ src/main/java/com/rest/webservices/restfulwebservices/service/UserDaoService.java
@@ -0,0 +1,40 @@
+package com.rest.webservices.restfulwebservices.service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.springframework.stereotype.Component;
+
+import com.rest.webservices.restfulwebservices.user.User;
+
+@Component
+public class UserDaoService {
+	
+	private static List<User> users = new ArrayList<>();
+	private static int count = 0;
+	
+	static {
+		users.add(new User(count++, "John", LocalDate.now().minusYears(40)));
+		users.add(new User(count++, "Steve", LocalDate.now().minusYears(30)));
+		users.add(new User(count++, "Ryan", LocalDate.now().minusYears(35)));
+		users.add(new User(count++, "George", LocalDate.now().minusYears(20)));
+	}
+	
+	public List<User> findAll() {
+		return users;
+	}
+	
+	public User findOne(int id) {
+		Predicate<? super User> predicate = user -> user.getId().equals(id); 
+		return users.stream().filter(predicate).findFirst().get();
+	}
+	
+	public User save(User user) {
+		user.setId(count++);
+		users.add(user);
+		return user;
+	}
+	
+}
