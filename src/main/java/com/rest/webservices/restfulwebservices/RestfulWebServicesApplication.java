1bd555eb4f4ad173883ad128e8399bb8bc5e7177
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-11-12 10:33:21 -0800

    Update main class

--- src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,6 +3,8 @@ package com.rest.webservices.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// run the application
+
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
