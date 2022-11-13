7b0c352f4f951258aae039e91908f100f130cace
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-11-13 13:21:20 -0800

    Added 404 response status

--- src/main/java/com/rest/webservices/restfulwebservices/controller/exception/UserNotFoundException.java
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/exception/UserNotFoundException.java
@@ -1,6 +1,10 @@
 package com.rest.webservices.restfulwebservices.controller.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 @SuppressWarnings("serial")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 
 	public UserNotFoundException(String message) {
