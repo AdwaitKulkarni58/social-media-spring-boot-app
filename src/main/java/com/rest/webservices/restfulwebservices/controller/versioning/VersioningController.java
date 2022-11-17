4082e96290e9ac4808489f79c8040d60bae1a61e
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-11-17 10:47:27 -0800

    Create new class for new API version

--- src/main/java/com/rest/webservices/restfulwebservices/controller/versioning/VersioningController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/controller/versioning/VersioningController.java
@@ -1,8 +1,13 @@
 package com.rest.webservices.restfulwebservices.controller.versioning;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningController {
 
+	@GetMapping("/v1/person")
+	public Person getPerson() {
+		return new Person("First Last");
+	}
 }
